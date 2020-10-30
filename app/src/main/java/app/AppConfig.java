72c9e7b8390ccc776c38f72b5bfb18ee96a48613
Mohamed Abdelmaxod <mido99ahmed14@gmail.com>
2020-10-30 18:52:16 +0200

    Milestone 1 /Android

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,10 @@
+package app;
+
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.92.1/project/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.92.1/project/register.php";
+}
